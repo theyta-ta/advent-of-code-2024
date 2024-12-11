4629bd1a58aee37417e2ec2d603ffdb0bb811798
theyta-ta <hanamora38@gmail.com>
2024-12-11 02:14:47 +0000

    rewrite of day03

--- rust/src/days/day03.rs
+++ rust/src/days/day03.rs
@@ -1,8 +1,22 @@
-use crate::file_ops;
 use regex::Regex;
 
+use std::fs;
+use std::io::{BufReader, Read /* for `bytes` */};
+
+// use this method instead of file_ops to get rid of making everything oneline in part b
+fn read_day() -> String {
+    String::from_utf8(
+        BufReader::new(fs::File::open("data/data03.txt").unwrap())
+            .bytes()
+            .map(|x| x.unwrap())
+            .filter(|&c| c != b'\n')
+            .collect::<Vec<_>>(),
+    )
+    .unwrap()
+}
+
 pub fn solve() {
-    let data = file_ops::read_day(3);
+    let data = read_day();
     println!("Part a: {}", part_a(&data));
     println!("Part b: {}", part_b(&data));
 }
@@ -19,10 +33,12 @@ fn part_a(data: &str) -> u32 {
 
 fn part_b(data: &str) -> u32 {
     let re = Regex::new(r"(?:^|do\(\))(.*?)(?:don't\(\).*?|$)").unwrap();
-    // i know you can do better than recreating the string but w/out `\n`s.
-    // i just dont want to.
-    re.captures_iter(&data.lines().collect::<Vec<_>>().join(""))
-        .map(|c| c.extract::<1>().1[0])
-        .map(part_a)
+    re.captures_iter(data)
+        .map(|c| {
+            // dont fucking do `part_a(c.extract::<1>().1[0])`.
+            // dont you fucking dare.
+            let (_, [ret]) = c.extract();
+            part_a(ret)
+        })
         .sum()
 }
